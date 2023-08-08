affb7b697b42be5852f27a449bc26a2a75f30ef4
Mirzomiddin <141604581+MirzakhanDeveloper@users.noreply.github.com>
2023-08-08 12:00:58 +0500

    INFRA : Upload Project

--- Program.cs
+++ Program.cs
@@ -1,10 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Enter 1st number :");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter 2nd number :");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-
-string customer = 
-    firstNumber >= secondNumber ? "True" : "False";
-    Console.Write("Permission : " + customer);
+Console.WriteLine("Switch operatori");
 
