def6f5c5414b222af69f1b439f1bd755c01332c5
Mirzomiddin <141604581+MirzakhanDeveloper@users.noreply.github.com>
2023-08-08 16:16:45 +0500

    INFRA : Upload Project

--- Program.cs
+++ Program.cs
@@ -1,47 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Switch operatori");
-Console.WriteLine("Calculator");
-Console.Write("Type 1st number : ");
-string firstNumber =Convert.ToString(Console.ReadLine());
-int a = Convert.ToInt32(firstNumber);
+﻿Console.WriteLine("Switch expression");
+Console.Write("Enter 1st number : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int aA = (int)a;
+
+Console.Write("Enter 2nd number : ");
+int b = Convert.ToInt32(Console.ReadLine());
+int bB = (int)b;
 
 
-Console.Write("Type 2nd number : ");
-string secondNumber =Convert.ToString(Console.ReadLine());
-int b = Convert.ToInt32(secondNumber);
-
-Console.Write("Operator (+ , - , * , / ) : ");
-char opA = Convert.ToChar(Console.ReadLine());
-
-switch (opA) {
-    case '+' : 
-        Console.WriteLine($"{a} + {b} = {a + b}");
-        break;
-    case '-' : 
-        Console.WriteLine($"{a} - {b} = {a - b}");
-        break;
-    case '*' : 
-        Console.WriteLine($"{a} * {b} = {a * b}");
-        break;
-    case '/' : 
-        Console.WriteLine($"{a} / {b} = {a / b}");
-        break;
-    case '%' : 
-        Console.WriteLine($"{a} % {b} = {a % b}");
-        break;
-    default : 
-        Console.WriteLine("Nothing to do");
-        break;
-}
-
-
-// Console.Write("Type is operator : + , -, * , / : ");
-// char countnumber = Convert.ToChar(Console.Read());
-
-// Console.WriteLine("Type 2nd number : ");
-// int secondNumber = Convert.ToInt32(Console.Read());
-
-// Console.WriteLine($"{firstNumber} {countnumber} {secondNumber} = {firstNumber + secondNumber}");
+Console.Write("Enter operation : ");
+string c = Convert.ToString(Console.ReadLine());
 
 
 
+string result = c switch {
+    "+" => $"{aA} + {bB} = {aA + bB}",
+    "-" => $"{aA} - {bB} = {aA - bB}",
+    "*" => $"{aA} * {bB} = {aA * bB}",
+    "/" => $"{aA} / {bB} = {aA / bB}",
+    _ => "Nothing to done",
+};
+Console.WriteLine(result);
