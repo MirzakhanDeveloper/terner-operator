a349174f7e85c1e51f645f633f90e0ba4e2248e4
Mirzomiddin <141604581+MirzakhanDeveloper@users.noreply.github.com>
2023-08-08 14:55:15 +0500

    INFRA: Upload project

--- Program.cs
+++ Program.cs
@@ -1,3 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Switch operatori");
+Console.WriteLine("Calculator");
+Console.Write("Type 1st number : ");
+string firstNumber =Convert.ToString(Console.ReadLine());
+int a = Convert.ToInt32(firstNumber);
+
+
+Console.Write("Type 2nd number : ");
+string secondNumber =Convert.ToString(Console.ReadLine());
+int b = Convert.ToInt32(secondNumber);
+
+Console.Write("Operator (+ , - , * , / ) : ");
+char opA = Convert.ToChar(Console.ReadLine());
+
+switch (opA) {
+    case '+' : 
+        Console.WriteLine($"{a} + {b} = {a + b}");
+        break;
+    case '-' : 
+        Console.WriteLine($"{a} - {b} = {a - b}");
+        break;
+    case '*' : 
+        Console.WriteLine($"{a} * {b} = {a * b}");
+        break;
+    case '/' : 
+        Console.WriteLine($"{a} / {b} = {a / b}");
+        break;
+    case '%' : 
+        Console.WriteLine($"{a} % {b} = {a % b}");
+        break;
+    default : 
+        Console.WriteLine("Nothing to do");
+        break;
+}
+
+
+// Console.Write("Type is operator : + , -, * , / : ");
+// char countnumber = Convert.ToChar(Console.Read());
+
+// Console.WriteLine("Type 2nd number : ");
+// int secondNumber = Convert.ToInt32(Console.Read());
+
+// Console.WriteLine($"{firstNumber} {countnumber} {secondNumber} = {firstNumber + secondNumber}");
+
+
 
